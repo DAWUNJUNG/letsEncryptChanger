745779fe832e9223b6259d2164b92a6be1194869
dawun <dawun@lacy.co.kr>
2023-07-23 16:28:22 +0900

    test

--- main.py
+++ main.py
@@ -16,30 +16,31 @@ class autoRenewLetsEncrypt:
         self.domain = os.getenv('DOMAIN')
         self.haproxyPath = os.getenv('HAPROXY_PATH') + '/haproxy.cfg'
         self.todayDate = date.today()
-        self.log = ''
+        self.log_message = ''
 
     def start(self):
-        try:
-            self.log = self.log + "============ Start Renew Let's Encrypt ============\n"
-            if not self.renewLetsEncrypt():
-                return False
-            self.log = self.log + "============ End Renew Let's Encrypt ============\n"
-            self.log = self.log + "============ Start Make Site Pem ============\n"
-            if not self.makeSitePem():
-                return False
-            self.log = self.log + "============ End Make Site Pem ============\n"
-            self.log = self.log + "============ Start Change Encrypt Dir Name ============\n"
-            if not self.changeEncryptDirName():
-                return False
-            self.log = self.log + "============ End Change Encrypt Dir Name ============\n"
-            self.log = self.log + "============ Start Modify Proxy Config ============\n"
-            if not self.modifyProxyConfig():
-                return False
-            self.log = self.log + "============ End Modify Proxy Config ============\n"
+        renewLetsEncryptResult = False
+        makeSitePemResult = False
+        changeEncryptDirNameResult = False
 
-            return True
-        except():
-            return False
+        self.log("============ Start Renew Let's Encrypt ============\n")
+        renewLetsEncryptResult = self.renewLetsEncrypt()
+        self.log("============ End Renew Let's Encrypt ============\n")
+
+        if renewLetsEncryptResult:
+            self.log("============ Start Make Site Pem ============\n")
+            makeSitePemResult = self.makeSitePem()
+            self.log("============ End Make Site Pem ============\n")
+
+        if makeSitePemResult:
+            self.log("============ Start Change Encrypt Dir Name ============\n")
+            changeEncryptDirNameResult = self.changeEncryptDirName()
+            self.log("============ End Change Encrypt Dir Name ============\n")
+
+        if changeEncryptDirNameResult:
+            self.log("============ Start Modify Proxy Config ============\n")
+            self.modifyProxyConfig()
+            self.log("============ End Modify Proxy Config ============\n")
 
     def renewLetsEncrypt(self):
         try:
@@ -49,7 +50,7 @@ class autoRenewLetsEncrypt:
                       f"/root/.secrets/certbot-cloudflare.ini -d {self.domain} -d *.{self.domain}"
             command_result = os.popen(command).read()
 
-            self.log = self.log + command_result + '\n'
+            self.log(command_result + '\n')
 
             if command_result.find("Error creating new order"):
                 return False
@@ -63,9 +64,10 @@ class autoRenewLetsEncrypt:
     def makeSitePem(self):
         try:
             # site.pem 파일 생성
-            sitePemResult = os.popen(f"cat {self.encryptLivePath}/cert.pem {self.encryptLivePath}/chain.pem {self.encryptLivePath}/privkey.pem > {self.encryptLivePath}/site.pem").read()
+            sitePemResult = os.popen(
+                f"cat {self.encryptLivePath}/cert.pem {self.encryptLivePath}/chain.pem {self.encryptLivePath}/privkey.pem > {self.encryptLivePath}/site.pem").read()
 
-            self.log = self.log + sitePemResult + '\n'
+            self.log(sitePemResult + '\n')
 
             if sitePemResult != '':
                 return False
@@ -82,16 +84,17 @@ class autoRenewLetsEncrypt:
         try:
             # live, archive 디렉토리명 변경
             changeDirLive = os.popen(f"mv {self.encryptLivePath}/ {self.encryptLivePath}-{self.todayDate}").read()
-            self.log = self.log + changeDirLive + '\n'
+            self.log(changeDirLive + '\n')
             if changeDirLive != '':
                 return False
-            changeDirArchive = os.popen(f"mv {self.encryptArchivePath}/ {self.encryptArchivePath}-{self.todayDate}").read()
-            self.log = self.log + changeDirArchive + '\n'
+            changeDirArchive = os.popen(
+                f"mv {self.encryptArchivePath}/ {self.encryptArchivePath}-{self.todayDate}").read()
+            self.log(changeDirArchive + '\n')
             if changeDirArchive != '':
                 return False
             # renew 파일명 변경
             changeDirRenew = os.popen(f"mv {self.encryptRenewPath}/ {self.encryptRenewPath}-{self.todayDate}").read()
-            self.log = self.log + changeDirRenew + '\n'
+            self.log(changeDirRenew + '\n')
             if changeDirRenew != '':
                 return False
 
@@ -105,22 +108,22 @@ class autoRenewLetsEncrypt:
 
             with open(f"{self.haproxyPath}", 'rt') as file:
                 proxy_cfg = file.read()
-                self.log = self.log + "==== Before Proxy File ====" + '\n'
-                self.log = self.log + proxy_cfg + '\n'
+                self.log("==== Before Proxy File ====" + '\n')
+                self.log(proxy_cfg + '\n')
                 proxy_cfg = proxy_cfg.replace(f"{self.domain}-2023-07-21", f"{self.domain}-{self.todayDate}")
                 re.sub(f"/{self.domain}-^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/",
                        f"{self.domain}-{self.todayDate}", proxy_cfg)
-                self.log = self.log + "==== After Proxy File ====" + '\n'
-                self.log = self.log + proxy_cfg + '\n'
+                self.log("==== After Proxy File ====" + '\n')
+                self.log(proxy_cfg + '\n')
                 file.close()
 
             with open(f"{self.haproxyPath}", 'wt') as file:
                 file.write(proxy_cfg)
                 file.close()
 
-            self.log = self.log + "==== Proxy Restart ====" + '\n'
+            self.log("==== Proxy Restart ====" + '\n')
             haproxyRestartResult = os.popen('systemctl restart haproxy').read()
-            self.log = self.log + haproxyRestartResult + '\n'
+            self.log(haproxyRestartResult + '\n')
             if haproxyRestartResult != '':
                 return False
 
@@ -130,22 +133,29 @@ class autoRenewLetsEncrypt:
 
     # 메일 발송 function
     def mail_send(self):
-        smtp = smtplib.SMTP('smtp.gmail.com', 587)
-        smtp.ehlo()
-        smtp.starttls()  # TLS 사용시 필요
-        smtp.login(os.getenv('GOOGLE_ID'), os.getenv('GOOGLE_APP_PW'))
+        print(self.log_message)
 
-        msg = MIMEText(self.log)
-        msg['To'] = os.getenv('DESTINATION_EMAIL')
-        msg['From'] = os.getenv('SOURCE_EMAIL')
-        msg['Subject'] = f"{self.domain} 인증서 교체 자동화 ({datetime.now().strftime('%Y-%m-%d %H:%M:%S')})"
-        smtp.sendmail(os.getenv('SOURCE_EMAIL'), os.getenv('DESTINATION_EMAIL'), msg.as_string())
+        # smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        # smtp.ehlo()
+        # smtp.starttls()  # TLS 사용시 필요
+        # smtp.login(os.getenv('GOOGLE_ID'), os.getenv('GOOGLE_APP_PW'))
+        #
+        # msg = MIMEText(self.log_message)
+        # msg['To'] = os.getenv('DESTINATION_EMAIL')
+        # msg['From'] = os.getenv('SOURCE_EMAIL')
+        # msg['Subject'] = f"{self.domain} 인증서 교체 자동화 ({datetime.now().strftime('%Y-%m-%d %H:%M:%S')})"
+        # smtp.sendmail(os.getenv('SOURCE_EMAIL'), os.getenv('DESTINATION_EMAIL'), msg.as_string())
+
+    # 로그 작성 function
+    def log(self, message):
+        self.log_message = self.log_message + message
 
 
 if __name__ == '__main__':
     renewClass = autoRenewLetsEncrypt()
+    renewClass.start()
     renewClass.mail_send()
-    if renewClass.start():
-        print('성공')
-    else:
-        print('실패')
+    # if renewClass.start():
+    #     print('성공')
+    # else:
+    #     print('실패')
