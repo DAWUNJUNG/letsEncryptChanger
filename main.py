12f656e0092187843f3810319e95e8be0bb5598b
dawun <dawun@lacy.co.kr>
2023-07-23 16:31:14 +0900

    수정버그 수정

--- main.py
+++ main.py
@@ -151,8 +151,10 @@ class autoRenewLetsEncrypt:
 
 if __name__ == '__main__':
     renewClass = autoRenewLetsEncrypt()
-    renewClass.mail_send()
+
     if renewClass.start():
         print('성공')
     else:
         print('실패')
+
+    renewClass.mail_send()
