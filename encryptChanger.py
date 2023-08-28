0236ef456ff6e8ea62ce509172498e6e9830d0c0
DAWUN <dawun@lacy.co.kr>
2023-08-28 10:30:50 +0900

    test

--- encryptChanger.py
+++ encryptChanger.py
@@ -143,11 +143,11 @@ class autoRenewLetsEncrypt:
             renewalPathSplit = self.encryptRenewalPath.rsplit('.', 1)
 
             # 변경일 디렉토리 삭제 대상에서 제외
-            if f"{self.encryptLivePath}-{self.todayDate}" in liveFileList:
+            if self.encryptLivePath + '-' + self.todayDate in liveFileList:
                 liveFileList.remove(f"{self.encryptLivePath}-{self.todayDate}")
-            if f"{self.encryptArchivePath}-{self.todayDate}" in archiveFileList:
+            if self.encryptArchivePath + '-' + self.todayDate in archiveFileList:
                 archiveFileList.remove(f"{self.encryptArchivePath}-{self.todayDate}")
-            if f"{renewalPathSplit[0]}-{self.todayDate}.{renewalPathSplit[1]}" in archiveFileList:
+            if renewalPathSplit[0] + '-' + self.todayDate + '.' + renewalPathSplit[1] in archiveFileList:
                 renewalFileList.remove(f"{renewalPathSplit[0]}-{self.todayDate}.{renewalPathSplit[1]}")
 
             # live 디렉토리 작업
